dc30b9991a763b3f6ae809dfa1f1b3570386f1e1
mhtrimech <mhtrimech@solocal.com>
2020-03-17 16:25:27 +0100

    Init project & template.

---
+++ src/assets/css/css.js
@@ -0,0 +1,14 @@
+// CSS FILES
+import "./bootstrap.min.css";
+import "./animate.css";
+import "./jquery-ui.min.css";
+import "./meanmenu.min.css";
+import "./font-awesome.min.css";
+import "./pe-icon-7-stroke.css";
+import "./flaticon.css";
+import "./venobox/venobox.css";
+import "./nivo-slider.css";
+import "./preview.css";
+import "./owl.carousel.css";
+import "./style.css";
+import "./responsive.css";
