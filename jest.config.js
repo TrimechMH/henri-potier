762934f2803a5be2e2bac23197302e581817d485
mhtrimech <mhtrimech@solocal.com>
2020-03-18 12:42:08 +0100

    Main, App test. Common Components Test.

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,9 @@
 module.exports = {
-  preset: "@vue/cli-plugin-unit-jest"
+    preset: '@vue/cli-plugin-unit-jest',
+    testMatch: [
+        '**/src/**/*.spec.(js|jsx|ts|vue|tsx.vue)|**/__tests__/*.(js|jsx|ts|tsx)'
+    ],
+    setupFiles: ['./src/setup-jest.js'],
+    collectCoverage: true,
+    collectCoverageFrom: ['**/src/**/*.{js,vue}', '!**/src/**/{index,routes,stores,router}.{js,vue}', '!**/(node_modules|assets)/**'],
 };
