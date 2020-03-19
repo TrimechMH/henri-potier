f7d827734533baf5aafa694727e482f0b0853df4
mhtrimech <mhtrimech@solocal.com>
2020-03-19 22:29:45 +0100

    Catalogue.

---
+++ src/modules/shopping/components/CatalogueFilters.spec.js
@@ -0,0 +1,69 @@
+import { shallowMount, createLocalVue } from '@vue/test-utils';
+import Vuex from 'vuex';
+import Vue from 'vue';
+import CatalogueFilters from './CatalogueFilters';
+import VueRouter from 'vue-router';
+import lodash from 'lodash';
+import VueLodash from 'vue-lodash';
+
+const localVue = createLocalVue();
+const router = new VueRouter({
+    history: true,
+});
+
+localVue.use(Vuex);
+localVue.use(VueRouter);
+localVue.use(VueLodash, {name: 'custom', lodash: lodash});
+
+let getStore = (mutations) => {
+    return new Vuex.Store({
+        modules: {
+            shoppingStore: {
+                namespaced: true,
+                mutations
+            }
+        }
+    });
+};
+
+describe('CatalogueFilters', () => {
+
+    let store;
+    let mutations;
+    let wrapper;
+
+    beforeEach(() => {
+        mutations = {
+            setSearchValue: jest.fn()
+        };
+
+        store = getStore(mutations);
+        wrapper = shallowMount(CatalogueFilters, {router, store, localVue});
+    });
+
+    // Inspect the raw component options
+    it('Should be created', () => {
+        expect(CatalogueFilters).toBeDefined();
+        expect(CatalogueFilters.name).toEqual('catalogue-filters');
+        expect(typeof CatalogueFilters.methods.setSearchValue).toEqual('function');
+        expect(wrapper.vm.$data.search).toBeNull();
+    });
+
+    it('Should commit "setSearchValue" on input changes', async () => {
+        const input = wrapper.find('.price-slider-amount input#search');
+        input.setValue('Hello');
+        await wrapper.vm.$nextTick();
+        expect(wrapper.vm.$data.search).toEqual('Hello');
+        expect(mutations.setSearchValue).toBeCalled();
+    });
+
+    it('Should commit "setSearchValue" on reset search click', async () => {
+        const resetBtn = wrapper.find('.widget-buttom input');
+        resetBtn.trigger('click');
+
+        await wrapper.vm.$nextTick();
+        expect(wrapper.vm.$data.search).toEqual('');
+        expect(mutations.setSearchValue).toBeCalled();
+    });
+
+});
